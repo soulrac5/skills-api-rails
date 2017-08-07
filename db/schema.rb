fae09683c359f69f14230f907eee61e513e5833d
jorge david hernandez diaz <jdaviderb@gmail.com>
2017-08-07 15:06:52 -0400

    add columns authentication to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803173356) do
+ActiveRecord::Schema.define(version: 20170807190454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -73,6 +73,8 @@ ActiveRecord::Schema.define(version: 20170803173356) do
     t.string "city"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "token"
+    t.date "expire"
     t.index ["rol_id"], name: "index_users_on_rol_id"
   end
 
