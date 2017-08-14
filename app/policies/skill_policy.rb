80fa57ff69b83949a8e77e0e7d05436e0e408281
jorge david hernandez diaz <jdaviderb@gmail.com>
2017-08-14 13:01:17 -0400

    fix polices

--- app/policies/skill_policy.rb
+++ app/policies/skill_policy.rb
@@ -1,11 +1,11 @@
 class SkillPolicy < ApplicationPolicy
   def create?
-    @user.try(:rol_id) == 1
+    @user.rol_id == 1
   end
   def update?
-    @user.try(:rol_id) == 1
+    @user.rol_id == 1
   end
   def destroy?
-    @user.try(:rol_id) == 1
+    @user.rol_id == 1
   end
 end
--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -1,15 +1,15 @@
 class UserPolicy < ApplicationPolicy
   #Authorization user CRUDS
   def create?
-    @user.try(:rol_id) == 1
+    @user.rol_id == 1
   end
   def update?
-    @user.try(:rol_id) == 1
+    @user.rol_id == 1
   end
   def destroy?
-    @user.try(:rol_id) == 1
+    @user.rol_id == 1
   end
   def tags?
-    @user.try(:rol_id) == 1 || @user.try(:rol_id) == 3
+    @user.rol_id == 1 || @user.rol_id == 3
   end
 end
